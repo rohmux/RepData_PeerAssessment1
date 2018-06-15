5f466deaa6dcf5d464458c155f9a0ef652d25ca7
rohmux@gmail.com <Tony2301>
2018-06-15 15:01:43 +0200

    solved all questions, now ready for RMarkdown

--- playground.R
+++ playground.R
@@ -5,8 +5,8 @@ Sys.setlocale("LC_ALL","English")
 datadir <- "data"
 datafilename <- "activity.zip"
 datasource <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
-wDays <- list('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
-allDays <- list('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+wDays <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
+allDays <- data.frame(day = c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'), dayNumber = c(1,2,3,4,5,6,7))
 
 if (file.exists(datadir)){
 } else {
@@ -18,23 +18,26 @@ download.file(datasource, paste(datadir, "/", datafilename, sep=""), mode="wb")
 temp <- unz(paste(datadir, "/", datafilename, sep=""), "activity.csv")
 df <- read.csv(temp)
 timeString <- sprintf("%04d", df$interval, df$interval)
-df$date <- as.POSIXct(paste(df$date, " ", substr(timeString, 0, 2), ":", substr(timeString, 3, 4), sep=""))
+df$datetime <- as.POSIXct(paste(df$date, " ", substr(timeString, 0, 2), ":", substr(timeString, 3, 4), sep=""))
 df$interval <- paste(substr(timeString, 0, 2), ":", substr(timeString, 3, 4), sep="")
 df$day <- weekdays(as.Date(df$date))
 df$wdays <- with(df, ifelse(df$day %in% wDays, "weekday", "weekend"))
 
-sumStepsPerDay <- aggregate(df[,c('steps')], by=list(df$day), FUN=sum, na.rm=TRUE)
+sumStepsPerDay <- aggregate(df[,c('steps')], by=list(df$date), FUN=sum, na.rm=TRUE)
+sumStepsPerDay$day <- weekdays(as.Date(sumStepsPerDay$Group.1))
+sumStepsPerDay$wDay <- with(df, ifelse(sumStepsPerDay$day %in% wDays, "weekday", "weekend"))
+qplot(sumStepsPerDay$x, xlab = "Steps per Day", ylab = "Count", main = "Histogramm of Steps taken per Day", bins= "20")
 
-meanStepsPerDay <- aggregate(df[,c('steps')], by=list(df$day), FUN=mean, na.rm=TRUE)
+meanStepsPerDay <- aggregate(sumStepsPerDay[,c('x')], by=list(sumStepsPerDay$day), FUN=mean, na.rm=TRUE)
 
-medianStepsPerDay <- aggregate(df[,c('steps')], by=list(df$day), FUN=median, na.rm=TRUE)
+medianStepsPerDay <- aggregate(sumStepsPerDay[,c('x')], by=list(sumStepsPerDay$day), FUN=median, na.rm=TRUE)
 
 meanStepsPerInterval <- aggregate(df[,c('steps')], by=list(df$interval), FUN=mean, na.rm=TRUE)
+ticks <- meanStepsPerInterval[seq(1, nrow(meanStepsPerInterval), 24),][,1]
+ggplot(data=meanStepsPerInterval, aes(x=Group.1, y=x, group=1)) + geom_line() + scale_x_discrete(breaks = c(ticks))
 
-ggplot(data=meanStepsPerInterval, aes(x=Group.1, y=x, group=1)) + geom_line()
+highestInterval <- meanStepsPerInterval[ meanStepsPerInterval$x == max(meanStepsPerInterval$x),]
 
+countNA <- sum(is.na(df$steps))
 
-#meanStepsPerDay <- aggregate(df[,c('steps')], by=list(as.Date(df$date)), FUN=mean, na.rm=FALSE)
-#meanStepsPerDay$day <- weekdays(as.Date(meanStepsPerDay$Group.1))
-
-#ggplot(data=meanStepsPerDay, aes(x=Group.1, y=x, group=1)) + geom_line() + scale_x_date(date_breaks = "1 weeks")
+df$steps <- with(df, ifelse(is.na(df$steps), aggregate(df[,c('steps')], by=list(df$interval), FUN=mean, na.rm=TRUE)[,2], df[,1]))
